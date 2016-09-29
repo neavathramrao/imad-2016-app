03da5ed77958a60f4f42a28d3751324bbf1e8509
neavathramrao <ram.ramaraob4u@gmail.com>
2016-09-29 16:26:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -90,17 +90,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get("/article-one", function(req, res) {
-  res.send(createTemplate(articleOne));
+app.get("/:articleName", function(req, res) {
+    var articleName = req.params.articleName;
+  res.send(createTemplate(articcles[articleName]));
 } );
 
-app.get("/article-two", function(req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-} );
-
-app.get("/article-three", function(req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-} );
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
