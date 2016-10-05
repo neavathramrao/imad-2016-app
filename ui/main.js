061e004fd2747e80b5240d634418f44292dcac6f
neavathramrao <ram.ramaraob4u@gmail.com>
2016-10-05 12:21:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,9 @@ console.log('Loaded!');
 var element = document.getElementById("main-text");
 
 element.innerHTML = ("I Have changed this to new value");
+
+var img = document.getElementById("ram");
+
+img.onclick = function () {
+    img.style.marginLeft = '100px';
+};
