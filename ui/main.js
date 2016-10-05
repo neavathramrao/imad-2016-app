e250bd9253308baea3957fe28a37832fb093750b
neavathramrao <ram.ramaraob4u@gmail.com>
2016-10-05 23:26:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,22 @@ var button = document.getElementById("counter");
 var counter = 0;
 
 button.onclick = function () {
-    counter = counter + 1;
-    var span = document.getElementById("count");
-    span.innerHTML = counter.toString();
+    
+    //create a request object
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.readyState === 200) {
+                var counter = request.responseText;
+                var span = document.getElementById("count");
+                span.innerHTML = counter.toString();
+            }
+    }
+    // Not Done yet        
+        
+    };
+    //Make request
+    request.open("GET", "http://neavathramrao.imad.hasura-app.io/counter", true);
+    request.send(null);
+
 };
