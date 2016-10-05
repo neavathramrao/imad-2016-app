7959273a85896effb39d4b769f1eea5e6d43b6cc
neavathramrao <ram.ramaraob4u@gmail.com>
2016-10-05 23:02:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,10 @@
-console.log('Loaded!');
+//Counter code
 
-var element = document.getElementById("main-text");
+var button = document.getElementById("counter");
+var counter = 0;
 
-element.innerHTML = ("I Have changed this to new value");
-
-var img = document.getElementById("ram");
-
-var marginLeft = 0;
-function moveRight () {
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + "px";
-}
-
-img.onclick = function () {
-    var interval = setInterval(moveRight, 50);
+button.onclick = function () {
+    counter = counter + 1;
+    var span = document.getElementById("count");
+    span.innerHTML = counter.toString();
 };
