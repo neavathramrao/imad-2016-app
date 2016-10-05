dcde2f1925eaabaa709f1968e0f920efb2d45fc8
neavathramrao <ram.ramaraob4u@gmail.com>
2016-10-05 12:04:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,4 +2,4 @@ console.log('Loaded!');
 
 var element = document.getElementById("main-text");
 
-element.innerHTML("I Have changed this to new value");
+element.innerHTML = ("I Have changed this to new value");
